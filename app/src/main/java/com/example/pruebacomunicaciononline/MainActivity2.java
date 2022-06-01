5a16fd10f48dc45ddf2800553f6760d605624725
AlexYRaul <103044293+AlexYRaul@users.noreply.github.com>
2022-06-01 18:03:32 +0200

    pruebas

--- app/src/main/java/com/example/pruebacomunicaciononline/MainActivity2.java
+++ app/src/main/java/com/example/pruebacomunicaciononline/MainActivity2.java
@@ -3,8 +3,23 @@ package com.example.pruebacomunicaciononline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
+    ListView listView;
+    Button button;
+
+    List<String> roomList;
+
+    String playerName = "";
+    String roomName = "";
+
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
